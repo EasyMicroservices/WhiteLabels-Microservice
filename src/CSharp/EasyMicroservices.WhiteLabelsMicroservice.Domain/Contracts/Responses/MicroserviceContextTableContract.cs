4cb8809ce19fa7fe4255483a4a7ab6594fceb93f
Ali Yousefi <ali.visual.studio@gmail.com>
2023-07-06 11:42:24 +0330

    build domain

---
+++ src/CSharp/EasyMicroservices.WhiteLabelsMicroservice.Domain/Contracts/Responses/MicroserviceContextTableContract.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyMicroservices.WhiteLabelsMicroservice.Contracts.Responses
+{
+    public class MicroserviceContextTableContract
+    {
+        public string MicroserviceName { get; set; }
+        public long MicroserviceId { get; set; }
+
+        public string ContextName { get; set; }
+        public long ContextId { get; set; }
+
+        public string TableName { get; set; }
+        public long TableId { get; set; }
+    }
+}
