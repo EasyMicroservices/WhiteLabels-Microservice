08b91a90d0924952291017182c2cf1092c585a6a
Ali Yousefi <ali.visual.studio@gmail.com>
2023-07-06 11:42:16 +0330

    build database structure

---
+++ src/CSharp/EasyMicroservices.WhiteLabelsMicroservice.Database/Database/Schemas/ContextTableSchema.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyMicroservices.WhiteLabelsMicroservice.Database.Schemas
+{
+    public class ContextTableSchema
+    {
+        /// <summary>
+        /// table name
+        /// </summary>
+        public string TableName { get; set; }
+        /// <summary>
+        /// database name
+        /// </summary>
+        public string ContextName { get; set; }
+    }
+}
