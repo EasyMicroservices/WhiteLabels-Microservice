64359eed811771226db512b8494fcb404f4ac232
Ali Yousefi <ali.visual.studio@gmail.com>
2023-07-09 11:31:45 +0330

    fix property names

--- src/CSharp/EasyMicroservices.WhiteLabelsMicroservice.Domain/Contracts/Common/WhiteLabelContract.cs
+++ src/CSharp/EasyMicroservices.WhiteLabelsMicroservice.Domain/Contracts/Common/WhiteLabelContract.cs
@@ -4,6 +4,7 @@
     {
         public long Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public long? ParentId { get; set; }
     }
 }
